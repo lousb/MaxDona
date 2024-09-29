84243347cc9003804b192e2268ba5eb801868668
lousb <wyethtafe@gmail.com>
2024-09-29 18:18:01 +1000

    video tweak

--- src/components/pages/single/single.js
+++ src/components/pages/single/single.js
@@ -434,7 +434,7 @@ const renderSection = (section, index) => {
 
       >
         <div className="video-hover-svg-wrap">
-          <svg width="17" height="25" viewBox="0 0 17 25" fill="none" xmlns="http://www.w3.org/2000/svg" className="video-hover-arrow">
+          <svg width="17" height="25" viewBox="0 0 17 25" fill="none" xmlns="https://www.w3.org/2000/svg" className="video-hover-arrow">
           <path  fill-rule="evenodd" clip-rule="evenodd" d="M2.0078 4.99998H0.29C0.193334 4.99998 0 4.95453 0 4.77271V0.22727C0 0 0.348 0 0.580001 0H1.74023H6.38H6.38086H8.12023H11.2382H12.1809H16.2405V4.63965V6.37983V10.4395V10.4473H16.2435V16.2471C16.2435 16.4791 16.2435 16.8271 16.0053 16.8271H11.2411C11.0506 16.8271 11.0029 16.6338 11.0029 16.5371V10.7739C11.0009 10.7578 11 10.7427 11 10.7295V8.99603L6.39529 13.6006L6.39552 13.6008L3.92365 16.0726C3.82477 16.1715 3.67646 16.3198 3.50954 16.1529L0.171188 12.8146C0.037654 12.6811 0.0866662 12.5653 0.127864 12.5241L1.48753 11.1645L1.4873 11.1642L7.65128 5.00047H2.03023C2.02257 5.00047 2.01509 5.0003 2.0078 4.99998Z" fill="#181818"/>
           </svg>
         </div>
@@ -880,24 +880,25 @@ const CustomYouTubePlayer = ({ setVideoProgress, setVideoCurrentTime, setCopyLin
     }
   };
 
-  const onReady = (event) => {
-    event.target.addEventListener('onStateChange', (e) => {
-      if (e.data === window.YT.PlayerState.PLAYING) {
-        setIsPlaying(true);
-        setIsBuffering(false);
-        startProgressTracking(event.target);
-      } else if (e.data === window.YT.PlayerState.BUFFERING) {
-        setIsBuffering(true);
-      } else if(e.data === window.YT.PlayerState.ENDED) {
-        setIsPlaying(false);
-        setIsBuffering(false);
-        setIsPlayingProp(false);
-      } else {
-        setIsPlaying(false);
-        setIsBuffering(false);
-      }
-    });
-  };
+ const handleStateChange = (event) => {
+   const player = event.target;
+
+   if (event.data === window.YT.PlayerState.PLAYING) {
+     setIsPlaying(true);
+     setIsBuffering(false);
+     setIsPlayingProp(true);
+     startProgressTracking(player);
+   } else if (event.data === window.YT.PlayerState.BUFFERING) {
+     setIsBuffering(true);
+   } else if (event.data === window.YT.PlayerState.ENDED) {
+     setIsPlaying(false);
+     setIsBuffering(false);
+     setIsPlayingProp(false);
+   } else {
+     setIsPlaying(false);
+     setIsBuffering(false);
+   }
+ };
 
   const startProgressTracking = (player) => {
     const interval = 500;
@@ -1013,7 +1014,7 @@ const CustomYouTubePlayer = ({ setVideoProgress, setVideoCurrentTime, setCopyLin
       <YouTube
          videoId={extractVideoId(videoUrl)}
         opts={opts}
-        onReady={onReady}
+        onStateChange={handleStateChange}
         ref={playerRef}
         
       />
